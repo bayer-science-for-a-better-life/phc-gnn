5b1081409aa022742c62fd2c9bfaf3e2cd59bae6
tuanle618 <tuanle@hotmail.de>
2021-03-26 10:04:53 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setup(
     name='phc',
     version='0.1',
     packages=['phc', 'phc.hypercomplex', 'phc.quaternion', 'benchmarks'],
-    url='',
-    license='',
+    url='https://github.com/bayer-science-for-a-better-life/phc-gnn',
+    license='GPL-3',
     author='Tuan Le',
     author_email='tuan.le2@bayer.com',
-    description='Implementation of the paper "Parameterized Hypercomplex Message Passing Neural Networks.'
+    description='Implementation of the paper "Parameterized Hypercomplex Graph Neural Networks for Graph Classification.'
 )
